f1823553fd079a8302478d9214dfd9bc4d10447f
seanrhee <lee.sean.cw@gmail.com>
2022-09-10 05:44:53 +0000

    add test file, index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,25 @@
 const request = require('request');
 
-const args = process.argv.slice(2);
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error) {
-    console.log('ERROR >>');
-    console.log(error);
-    return;
-  }
+      callback(error, null);
+      // console.log('ERROR >>');
+      // console.log(error);
+      // return;
+    }
+  
+    const data = JSON.parse(body);
+  
+    if (data.length === 0) {
+      console.log('Breed not found.');
+      return;
+      // callback(error, null);
+    }
+  
+    callback(null, data[0].description);
+    // return data[0];
+  })
+}
 
-  const data = JSON.parse(body);
-
-  if (data.length === 0) {
-    console.log('Breed not found.');
-    return;
-  }
-  console.log(data[0]);
-})
+module.exports = fetchBreedDescription;
